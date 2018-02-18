3cadf43adf1c909f4343c50ae9638d32a3832103
kkshmz <kyeshimizu@gmail.com>
2018-02-18 22:51:55 +0900

    Add blotter and landing css

--- themes/Origami/static-src/index.js
+++ themes/Origami/static-src/index.js
@@ -24,3 +24,17 @@ $(".prj-img").ready(function() {
 
     // // $(this).find(".prj-left-line").toggleClass('active').removeClass('active');
     // $(this).find('.prj-left-line, .prj-right-line').toggleClass('active').removeClass('active');
+    var text = new Blotter.Text("Kye", {
+        family : "san-serif",
+        size : 60,
+        fill : "#171717"
+      });
+    
+      var material = new Blotter.ChannelSplitMaterial();
+      
+    
+      var blotter = new Blotter(material, { texts : text });
+    
+      var scope = blotter.forText(text);
+    
+      scope.appendTo(document.body);
